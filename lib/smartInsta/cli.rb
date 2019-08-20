96a6e89faa6b5987252d98a5e791785bcb581ac5
Corey H <c.l.halicke@gmail.com>
2019-08-20 13:29:54 +0000

    cli building

--- lib/smartInsta/cli.rb
+++ lib/smartInsta/cli.rb
@@ -2,6 +2,53 @@ class SmartInsta::CLI
 
   def call
     puts "Smart Insta Program"
+    get_first_last_name
+    get_username
+    main_menu_options
+    main_menu
+
   end
-  
+
+  def get_first_last_name
+    puts "Enter your first name."
+    @first_name = gets.strip.downcase
+    puts "Enter your last name."
+    @last_name =  gets.strip.downcase
+  end
+
+  def get_username
+    puts "Welcome #{@first_name} #{@last_name}. Enter your instagram username."
+    @insta_username = gets.strip.downcase
+  end
+
+  def main_menu_options
+    puts <<-DOC
+    Choose an option:
+      [1] Likes
+      [2] Comments
+      [3] My Hashtags
+      [4] My Keywords
+      [5] Settings
+
+    DOC
+
+  end
+
+  def main_menu
+    puts "main menu"
+    @menu_input = gets.strip.downcase
+    until @input = 'x'
+
+      case @input
+      when 'x'
+      when '1'
+      when '2'
+      when '3'
+      when '4'
+      when '5'
+        
+      end
+    end
+
+  end
 end
